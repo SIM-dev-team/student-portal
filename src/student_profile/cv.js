70bb077bbd0b9f2407f7d16fbf80cea89d3aa9ae
Sidhana <nazmansidhana@gmail.com>
2020-07-20 21:36:34 +0530

    my cv button page created

---
+++ src/student_profile/cv.js
@@ -0,0 +1,19 @@
+import React , {Component} from 'react'
+import 'bootstrap/dist/css/bootstrap.css';
+import './Student.css'
+
+class Mycv extends Component{
+    render(){
+
+        return(
+            <div className = "my_cv pull-left">
+                <h4>CV will be displayed here </h4>
+                   
+            </div>
+        )
+        
+        
+    }
+}
+
+export default Mycv
