86a38e99756cf2310e804b04b7eaaf0176f199de
Sidhana <nazmansidhana@gmail.com>
2020-07-22 14:49:04 +0530

    implmented adverts part into cards

--- src/student_profile/saved.js
+++ src/student_profile/saved.js
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 import './Student.css'
 import {ADVERTS} from './card'
 
+
 class Saved extends Component {
 
     constructor(props) {
@@ -14,23 +15,23 @@ class Saved extends Component {
     }
 
     render() {
-        const menu = this.state.ads.map((ad) => {
-            return (
-                <div key = {ad.id} className ="student_card card mb-3 card">
+        const menu = this.state.ads.map((ad) => 
+            <div key = {ad.id}>
+                <div className ="student_card container card mb-3 card">
                     <div className = "row company_information">
                         <div className = "company_image">
-                        <img  src = {ad.image} alt="Card image cap" style = {{height:'3cm' , width:'3cm'}}/>
+                        <img  src = {ad.image} alt="company profile" style = {{height:'3cm' , width:'3cm'}}/>
                         </div>
-                        <div classname = "company_details">
+                        <div className = "company_details">
                             <h5> {ad.name}</h5>
                             <a>{ad.email}</a>
                         </div>
                         <div className = "company_buttons ">
                             <button className = "button_link">
-                                Saved
+                                View More
                             </button>
                             <button className = "button_link">
-                                Notifications
+                                Saved
                             </button>
                         </div>
                     </div>
@@ -39,14 +40,18 @@ class Saved extends Component {
                         <h5 className ="card-title">{ad.jobrole}</h5>
                         <p className ="card-text">{ad.description}</p>
                     </div>
+
+
                 </div>
-            );
-        });
+            </div>
+        );
 
         return (
-            <div className="container">
-                <div className="row">
-                    {menu}
+            <div className="scroller container">
+                <h5>Saved Adverts</h5>
+                <div className="advert_card row ">
+                         {menu}
+                   
                 </div>
             </div>
         );
