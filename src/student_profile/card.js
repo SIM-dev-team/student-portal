a03fa891e14f3a7574b2841d11d7a7ae12a50c0f
Sidhana <nazmansidhana@gmail.com>
2020-07-22 14:47:33 +0530

    testing content of arrays

--- src/student_profile/card.js
+++ src/student_profile/card.js
@@ -3,18 +3,57 @@ export const ADVERTS = [
     {
       id: 0,
       name:'WSO2',
-      email:'www.WSO2.com',
-      image: '../images/company.png',
+      email:'www.WSO2@gmail.com',
+      image: '../image/company.png',
       jobrole: 'Business Analyst',
-      description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.'                        },
-
+      description:'description goes here'},
 
       {
         id: 1,
         name:'Virtusa',
-        email:'www.virtusa.com',
+        email:'www.virtusa@gmail.com',
         image: '../images/company.png',
         jobrole: 'Business Analyst',
-        description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.'                        },
+        description:'Description goes here.'},
+
+        {
+          id: 2,
+          name:'Cambio',
+          email:'www.cambio@gmail.com',
+          image: '../images/company.png',
+          jobrole: 'Business Analyst',
+          description:'Description goes here.'},
+
+          {
+            id: 3,
+            name:'Nable',
+            email:'www.nable@gmail.com',
+            image: '../images/company.png',
+            jobrole: 'Business Analyst',
+            description:'Description goes here.'},
+
+            {
+              id: 4,
+              name:'CodeGun',
+              email:'www.codegun@gmail.com',
+              image: '../images/company.png',
+              jobrole: 'Business Analyst',
+              description:'Description goes here.'},
+
+            {
+              id: 5,
+              name:'Zebra',
+              email:'www.zebra@gmail.com',
+              image: '../images/company.png',
+              jobrole: 'Business Analyst',
+              description:'Description goes here.'},
+
+          {
+              id: 6,
+              name:'Pearson',
+              email:'www.pearson@gmail.com',
+              image: '../images/company.png',
+              jobrole: 'Business Analyst',
+              description:'Description goes here.'},
   
 ];
