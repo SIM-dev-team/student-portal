ac55a272e79747945343534f0b3088597709472a
Hasantha malshan <hasanthamalshan07@gmail.com>
2020-08-14 23:39:05 +0530

    auth interface templates

---
+++ src/views/forgotPassword.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../App.css';
+
+
+function ForgotPassword() {
+
+    return (
+        <React.Fragment>
+            <div className="login-page">
+                <div className="login-form">
+                    <div className="login-header">Forgot Password</div>
+                </div>
+            </div>
+        </React.Fragment>
+    )
+}
+
+export default ForgotPassword
